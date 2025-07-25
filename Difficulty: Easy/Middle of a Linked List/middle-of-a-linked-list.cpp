dbc8ba6ce8f1ef4adec3758159d84a5f4dc0d63c
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-07-25 13:53:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.cpp
@@ -0,0 +1,29 @@
+/* Link list Node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+class Solution {
+  public:
+    /* Should return data of middle node. If linked list is empty, then -1 */
+    int getMiddle(Node* head) {
+        // code here
+        // using Node  = ListNode ;
+        if(head == NULL) return NULL;
+        //if(head->next == NULL) return head ;
+        Node* slow = head ;
+        
+        Node* fast = head ;
+        while(fast !=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow->data ;
+    }
+};
