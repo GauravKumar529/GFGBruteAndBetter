c7398fd43279f907ecdb9f03be1cf2742ed7182c
Persistor <rajputgaurav9582@gmail.com>
2026-01-31 10:25:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
@@ -1,21 +1,20 @@
-// User function Template for C++
-
 class Solution {
   public:
-  vector<string > res ; 
-  void fun (int num , string s , char lastDigit) {
-      if(num == s.size()) {
-          res.push_back(s) ;
-          return  ; 
-      }
-      fun(num , s + "0" , '0');
-      if(lastDigit != '1') fun( num , s+ "1"  , '1') ;
-      return; 
-  }
-    vector<string> generateBinaryStrings(int num) {
-        // Write your code
-        fun(num , "" , '0');
-        return res   ; 
+    vector<string> binstr(int n) {
+        // code here
+        vector<string  > res ;
+        
+        for(int i = 0; i < (1<< n) ;i++) {
+            // res.push_back();
+            string s = "";
+            
+            for(int bit = n - 1 ;bit>= 0 ;bit--) {
+                s += char('0' +  ((i>> bit ) & 1 ));
+            }
+            res.push_back(s);
+        }
+        return res ;
+        
         
     }
 };
